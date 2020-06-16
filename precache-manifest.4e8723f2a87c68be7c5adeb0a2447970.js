bb10281bd7285e4dd81cbcc9966b6cb1f3ed720c
rbogachova <margita@tut.by>
2020-06-16 15:04:56 -0700

    Updates

---
+++ precache-manifest.4e8723f2a87c68be7c5adeb0a2447970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6d007de29bc1ee657b5ea5a2757c9f",
+    "url": "/user-management/index.html"
+  },
+  {
+    "revision": "b7224a4dca261efb5e29",
+    "url": "/user-management/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7224a4dca261efb5e29",
+    "url": "/user-management/static/js/2.0e67ac3a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/user-management/static/js/2.0e67ac3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e7cd92fc5d6fdac57d",
+    "url": "/user-management/static/js/main.acf9584b.chunk.js"
+  },
+  {
+    "revision": "f95a8a3b71ed33f885ff",
+    "url": "/user-management/static/js/runtime-main.c48b3a8e.js"
+  }
+]);
