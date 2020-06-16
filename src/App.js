d8aa1658690538798a7e60f8212e6e078e795148
Rita Bogachov <rbogachov@usatech.com>
2020-06-15 21:26:48 -0700

    modals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import GridRow from "./GridRow";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 function App() {
     const [users, setUsers] = useState([]);
+    const [addUserModalActive, setAddUserModalActive] = useState(false);
 
     const loadUsers = () => {
         axios({
@@ -15,14 +18,69 @@ function App() {
             });
     };
 
+    const renderLoadUsersButton = () =>
+        <button className="btn btn-primary mb-3" onClick={loadUsers}>Load Users</button>;
+
+    const renderAddUserButton = () =>
+        <>
+            <button className="btn btn-primary mr-2 mb-3" onClick={handleShowModal}>Add User</button>
+            <Modal show={addUserModalActive} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add user</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <table>
+                        <tr>
+                            <td><strong>Name: </strong></td>
+                            <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Email: </strong></td>
+                            <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Address: </strong></td>
+                            <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Website: </strong></td>
+                            <td><input type="text"/></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Company: </strong></td>
+                            <td><input type="text"/></td>
+                        </tr>
+                    </table>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleAddUserButtonClick}>OK</Button>
+                    <Button variant="secondary" onClick={handleCloseModal}>Cancel</Button>
+                </Modal.Footer>
+            </Modal>
+        </>;
+
+    const handleShowModal = () => {
+        setAddUserModalActive(true);
+    };
+
+    const handleCloseModal = () => {
+        setAddUserModalActive(false);
+    };
+
+    const handleAddUserButtonClick = () => {
+        setAddUserModalActive(false);
+    };
+
+    const deleteUser = (userId) => {
+        const updatedUsers = users.filter(el => el.id !== userId);
+        setUsers(updatedUsers);
+    };
+
     return (
         <div className="container-fluid">
             <h2>Users</h2>
 
-            <button className="btn btn-primary mb-3"
-                    onClick={loadUsers}>
-                Load Users
-            </button>
+            {users.length === 0 ? renderLoadUsersButton() : renderAddUserButton()}
 
             <table className="table table-bordered">
                 {
@@ -32,10 +90,7 @@ function App() {
                         <th>ID</th>
                         <th>Name</th>
                         <th>Email</th>
-                        <th>Street</th>
-                        <th>Suite</th>
-                        <th>City</th>
-                        <th>Zip</th>
+                        <th>Address</th>
                         <th>Website</th>
                         <th>Company</th>
                         <th/>
@@ -47,7 +102,8 @@ function App() {
                 {
                     users.map(user =>
                         <GridRow key={user.id}
-                                 user={user}/>
+                                 user={user}
+                                 deleteUser={deleteUser}/>
                     )
                 }
                 </tbody>
--- src/GridRow.js
+++ src/GridRow.js
@@ -1,22 +1,44 @@
 import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function GridRow(props) {
     const [editModeEnabled, setEditModeEnabled] = useState(false);
     const [savedUser, setSavedUser] = useState(props.user);
     const [editingUser, setEditingUser] = useState(props.user);
+    const [deleteUserModalActive, setDeleteUserModalActive] = useState(false);
 
-    const onEditButtonClick = () => {
-        setEditingUser(savedUser);
-        setEditModeEnabled(true);
+    const onEnterPress = (e) => {
+        let code = e.keyCode || e.which;
+        if (code === 13)
+            onSaveButtonClick();
+    };
+
+    const onSaveButtonClick = () => {
+        setSavedUser(editingUser);
+        setEditModeEnabled(false);
     };
 
     const onCancelButtonClick = () => {
         setEditModeEnabled(false);
     };
 
-    const onSaveButtonClick = () => {
-        setSavedUser(editingUser);
-        setEditModeEnabled(false);
+    const onEditButtonClick = () => {
+        setEditingUser(savedUser);
+        setEditModeEnabled(true);
+    };
+
+    const handleShowModal = () => {
+        setDeleteUserModalActive(true);
+    };
+
+    const handleCloseModal = () => {
+        setDeleteUserModalActive(false);
+    };
+
+    const handleDeleteButtonClick = () => {
+        props.deleteUser(props.user.id);
+        setDeleteUserModalActive(false);
     };
 
     const renderEditModeCells = () =>
@@ -25,49 +47,72 @@ function GridRow(props) {
             <td>
                 <input type="text"
                        value={editingUser.name}
-                       onChange={e => setEditingUser({...editingUser, name: e.target.value})}/>
+                       onChange={e => setEditingUser({...editingUser, name: e.target.value})}
+                       onKeyPress={onEnterPress}/>
             </td>
             <td>
                 <input type="text"
                        value={editingUser.email}
-                       onChange={e => setEditingUser({...editingUser, email: e.target.value})}/>
+                       onChange={e => setEditingUser({...editingUser, email: e.target.value})}
+                       onKeyPress={onEnterPress}/>
             </td>
             <td>
-                <input type="text"
-                       value={editingUser.address.street}
-                       onChange={e => setEditingUser({
-                           ...editingUser,
-                           address: {...editingUser.address, street: e.target.value}
-                       })}/>
-            </td>
-            <td>
-                <input type="text"
-                       value={editingUser.address.suite}
-                       onChange={e => setEditingUser({
-                           ...editingUser,
-                           address: {...editingUser.address, suite: e.target.value}
-                       })}/>
-            </td>
-            <td>
-                <input type="text"
-                       value={editingUser.address.city}
-                       onChange={e => setEditingUser({
-                           ...editingUser,
-                           address: {...editingUser.address, city: e.target.value}
-                       })}/>
-            </td>
-            <td>
-                <input type="text"
-                       value={editingUser.address.zipcode}
-                       onChange={e => setEditingUser({
-                           ...editingUser,
-                           address: {...editingUser.address, zipcode: e.target.value}
-                       })}/>
+                <table>
+                    <tr>
+                        <td><strong>Street: </strong></td>
+                        <td>
+                            <input type="text"
+                                   value={editingUser.address.street}
+                                   onChange={e => setEditingUser({
+                                       ...editingUser,
+                                       address: {...editingUser.address, street: e.target.value}
+                                   })}
+                                   onKeyPress={onEnterPress}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>Suite: </strong></td>
+                        <td>
+                            <input type="text"
+                                   value={editingUser.address.suite}
+                                   onChange={e => setEditingUser({
+                                       ...editingUser,
+                                       address: {...editingUser.address, suite: e.target.value}
+                                   })}
+                                   onKeyPress={onEnterPress}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>City: </strong></td>
+                        <td>
+                            <input type="text"
+                                   value={editingUser.address.city}
+                                   onChange={e => setEditingUser({
+                                       ...editingUser,
+                                       address: {...editingUser.address, city: e.target.value}
+                                   })}
+                                   onKeyPress={onEnterPress}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>Zip: </strong></td>
+                        <td>
+                            <input type="text"
+                                   value={editingUser.address.zipcode}
+                                   onChange={e => setEditingUser({
+                                       ...editingUser,
+                                       address: {...editingUser.address, zipcode: e.target.value}
+                                   })}
+                                   onKeyPress={onEnterPress}/>
+                        </td>
+                    </tr>
+                </table>
             </td>
             <td>
                 <input type="text"
                        value={editingUser.website}
-                       onChange={e => setEditingUser({...editingUser, website: e.target.value})}/>
+                       onChange={e => setEditingUser({...editingUser, website: e.target.value})}
+                       onKeyPress={onEnterPress}/>
             </td>
             <td>
                 <input type="text"
@@ -75,32 +120,42 @@ function GridRow(props) {
                        onChange={e => setEditingUser({
                            ...editingUser,
                            company: {...editingUser.company, name: e.target.value}
-                       })}/>
+                       })}
+                       onKeyPress={onEnterPress}/>
             </td>
-
             <td>
-                <button className="btn btn-sm btn-light float-right fa fa-times"
-                        onClick={onCancelButtonClick}/>
-
-                <button className="btn btn-sm btn-light float-right fa fa-check"
-                        onClick={onSaveButtonClick}/>
+                <button className="btn btn-sm btn-light fa fa-check mr-2" onClick={onSaveButtonClick}/>
+                <button type="button" className="btn btn-sm btn-light fa fa-times" onClick={onCancelButtonClick}/>
             </td>
-        </>;
+        </>
+    ;
 
     const renderReadOnlyModeCells = () =>
         <>
             <td>{savedUser.id}</td>
             <td>{savedUser.name}</td>
-            <td>{savedUser.email}</td>
-            <td>{savedUser.address.street}</td>
-            <td>{savedUser.address.suite}</td>
-            <td>{savedUser.address.city}</td>
-            <td>{savedUser.address.zipcode}</td>
-            <td>{savedUser.website}</td>
+            <td><a href={`mailto:${savedUser.email}`}>{savedUser.email}</a></td>
+            <td>
+                <strong>Street:</strong> {savedUser.address.street}<br/>
+                <strong>Suite:</strong> {savedUser.address.suite}<br/>
+                <strong>City:</strong> {savedUser.address.city}<br/>
+                <strong>Zip:</strong> {savedUser.address.zipcode}
+            </td>
+            <td><a href={`http://${savedUser.website}`}>{savedUser.website}</a></td>
             <td>{savedUser.company.name}</td>
             <td>
-                <button className="btn btn-sm float-right fa fa-pencil"
-                        onClick={onEditButtonClick}/>
+                <button className="btn btn-sm btn-light fa fa-pencil mr-2" onClick={onEditButtonClick}/>
+                <button className="btn btn-sm btn-light fa fa-trash" onClick={handleShowModal}/>
+                <Modal show={deleteUserModalActive} onHide={handleCloseModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Are you sure you want to delete user?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{props.user.name}</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="primary" onClick={handleDeleteButtonClick}>OK</Button>
+                        <Button variant="secondary" onClick={handleCloseModal}>Cancel</Button>
+                    </Modal.Footer>
+                </Modal>
             </td>
         </>;
 
